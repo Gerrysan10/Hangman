b7265d2372c71821fc4435900e8562071d807a7e
Gerrysan10 <20460135@colima.tecnm.mx>
2024-05-28 13:51:03 -0600

    fin

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; // Cambia la importación aquí
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Stats from './components/Stats.tsx';
-import { StatsProvider } from './components/StatsContext.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/stats',
-    element: <Stats />
-  }
-]);
-
-// Utiliza ReactDOM.createRoot en lugar de ReactDOM.render
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <StatsProvider>
-      <RouterProvider router={router} />
-    </StatsProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
